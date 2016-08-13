39e367bc70e761674c733111cd6464ca11116838
Pavel <nnnemesis.gm@gmail.com>
2016-08-13 17:22:37 +0300

    some prepare work

---
+++ Assets/Scripts/OctTree1.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OctTree1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
