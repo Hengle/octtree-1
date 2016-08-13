b1c6a5b6ca91591922d5aa8ec10c633c1db58772
Pavel <nnnemesis.gm@gmail.com>
2016-08-13 20:56:30 +0300

    OctTree v1 insert done

--- Assets/Scripts/OctTree1.cs
+++ Assets/Scripts/OctTree1.cs
@@ -1,15 +1,85 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class OctTree1 : MonoBehaviour {
+namespace OctTree1
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public static class Vector3Helper
+    {
+        public static Vector3 MedianPoint(Vector3 vec1, Vector3 vec2)
+        {
+            return new Vector3((vec1.x + vec2.x) / 2, (vec1.y + vec2.y) / 2, (vec1.z + vec2.z) / 2);
+        }
+    }
+
+    public static class BoundsHelper
+    {
+        public static Vector3[] deltas = new Vector3[] { new Vector3(-1, -1, -1), new Vector3(-1, 1, -1), new Vector3(1, -1, -1), new Vector3(1, 1, -1)
+                , new Vector3(-1, -1, 1), new Vector3(-1, 1, 1), new Vector3(1, -1, 1), new Vector3(1, 1, 1)};
+
+        public static Bounds SubBounds(Bounds orig, Vector3 center, int index)
+        {
+            var delta = deltas[index];
+            var origSize = orig.size / 4;
+            var newCenter = (center + orig.center) / 2 + new Vector3(origSize.x * delta.x, origSize.y * delta.y, origSize.z * delta.z);
+            var newSize = -new Vector3(center.x * delta.x, center.y * delta.y, center.z * delta.z)
+                + new Vector3(orig.center.x * delta.x, orig.center.y * delta.y, orig.center.z * delta.z)
+                + orig.size / 2;
+            return new Bounds(newCenter, newSize);
+        }
+    }
+
+    public class OctNode
+    {
+        public static int SUB_NODES_COUNT = 8;
+
+        public Bounds Bounds;
+        public Vector3 Center;
+        public object[] SubNodes = new object[SUB_NODES_COUNT];
+
+        public void Insert(Vector3 pos, object obj)
+        {
+            int subNodesIndex = GetSubNodesIndex(pos);
+            var cell = SubNodes[subNodesIndex];
+            if(cell == null)
+            {
+                SubNodes[subNodesIndex] = new KeyValuePair<Vector3, object>(pos, obj);
+            }
+            else if(cell is KeyValuePair<Vector3, object>)
+            {
+                var existed = (KeyValuePair<Vector3, object>)cell;
+                var newNode = new OctNode { Center = Vector3Helper.MedianPoint(pos, existed.Key), Bounds = BoundsHelper.SubBounds(Bounds, Center, subNodesIndex) };
+                newNode.Insert(existed.Key, existed.Value);
+                newNode.Insert(pos, obj);
+                SubNodes[subNodesIndex] = newNode;
+            }
+            else if(cell is OctNode)
+            {
+                (cell as OctNode).Insert(pos, obj);
+            }
+        }
+
+        public int GetSubNodesIndex(Vector3 pos)
+        {
+            int index = 0;
+            if(Center.x >= pos.x)
+            {
+                index += 1;
+            }
+            if(Center.y >= pos.y)
+            {
+                index += 2;
+            }
+            if(Center.z >= pos.z)
+            {
+                index += 4;
+            }
+            return index;
+        }
+
+    }
+
 }
+
+
---
+++ Assets/Scripts/TestOctTree1.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using OctTree1;
+
+public class TestOctTree1 : MonoBehaviour
+{
+
+    [ContextMenu("RunTests")]
+    public void RunTests()
+    {
+        Debug.Log("------------------");
+        Debug.Log(BoundsHelper.SubBounds(new Bounds(Vector3.zero, Vector3.one), Vector3.zero, 0).ToString("F2"));
+        Debug.Log(BoundsHelper.SubBounds(new Bounds(Vector3.zero, Vector3.one), Vector3.zero, 1).ToString("F2"));
+        Debug.Log(BoundsHelper.SubBounds(new Bounds(Vector3.zero, Vector3.one), Vector3.zero, 2).ToString("F2"));
+        Debug.Log(BoundsHelper.SubBounds(new Bounds(Vector3.zero, Vector3.one), Vector3.zero, 3).ToString("F2"));
+        Debug.Log(BoundsHelper.SubBounds(new Bounds(Vector3.zero, Vector3.one), Vector3.zero, 4).ToString("F2"));
+        Debug.Log(BoundsHelper.SubBounds(new Bounds(Vector3.zero, Vector3.one), Vector3.zero, 5).ToString("F2"));
+        Debug.Log(BoundsHelper.SubBounds(new Bounds(Vector3.zero, Vector3.one), Vector3.zero, 6).ToString("F2"));
+        Debug.Log(BoundsHelper.SubBounds(new Bounds(Vector3.zero, Vector3.one), Vector3.zero, 7).ToString("F2"));
+        Debug.Log("------------------");
+        var v1 = new Vector3(0.25f, 0.25f, 0.25f);
+        Debug.Log(BoundsHelper.SubBounds(new Bounds(Vector3.zero, Vector3.one), v1, 0).ToString("F3"));
+        Debug.Log(BoundsHelper.SubBounds(new Bounds(Vector3.zero, Vector3.one), v1, 1).ToString("F3"));
+        Debug.Log(BoundsHelper.SubBounds(new Bounds(Vector3.zero, Vector3.one), v1, 2).ToString("F3"));
+        Debug.Log(BoundsHelper.SubBounds(new Bounds(Vector3.zero, Vector3.one), v1, 3).ToString("F3"));
+        Debug.Log(BoundsHelper.SubBounds(new Bounds(Vector3.zero, Vector3.one), v1, 4).ToString("F3"));
+        Debug.Log(BoundsHelper.SubBounds(new Bounds(Vector3.zero, Vector3.one), v1, 5).ToString("F3"));
+        Debug.Log(BoundsHelper.SubBounds(new Bounds(Vector3.zero, Vector3.one), v1, 6).ToString("F3"));
+        Debug.Log(BoundsHelper.SubBounds(new Bounds(Vector3.zero, Vector3.one), v1, 7).ToString("F3"));
+        Debug.Log("------------------");
+        Debug.Log(Vector3Helper.MedianPoint(Vector3.zero, Vector3.one));
+        Debug.Log(Vector3Helper.MedianPoint(new Vector3(1, 1, 1), new Vector3(2, 2, 2)));
+        Debug.Log(Vector3Helper.MedianPoint(new Vector3(0, 0, 1), new Vector3(0, 0, 2)));
+        Debug.Log("------------------");
+    }
+
+}
+
+
